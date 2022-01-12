44440be8e4c0cb79ab8ff97a8d8621205a0c25f0
souloss <23100994+souloss@users.noreply.github.com>
2022-01-13 00:48:27 +0800

    test: :test_tube: 更新 pypssh 测试计划

--- test/sshd_service/qos.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-# TODO 动态生成随机故障
-tc  qdisc  add  dev  ens192  root  netem  delay  100ms  10ms  30% loss 5%
--- test/test.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-docker-compose up --scale sshd=1000 -d
